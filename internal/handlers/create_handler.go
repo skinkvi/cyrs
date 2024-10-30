89004c9d511c73c21cb037c0d2c87098cf6c39a1
skinkvi <skinkvi228aye@gmail.com>
2024-10-30 16:50:49 +0500

    custom loan struct, finish create handlers

--- internal/handlers/create_handler.go
+++ internal/handlers/create_handler.go
@@ -29,3 +29,70 @@ func (h *Handler) CreateReaderHandler(c *gin.Context) {
 	h.Logger.Sugar().Info("Successful created reader: ", reader)
 	c.JSON(http.StatusCreated, reader)
 }
+
+func (h *Handler) CreateBookHandler(c *gin.Context) {
+	var book *models.Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		h.Logger.Sugar().Error("Falied to bing JSON: ", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := db.DB.Create(&book).Error; err != nil {
+		h.Logger.Sugar().Error("Falied to create book: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	h.Logger.Sugar().Info("Successful created reader: ", book)
+	c.JSON(http.StatusCreated, book)
+}
+
+func (h *Handler) CreateAuthorHandler(c *gin.Context) {
+	var author *models.Author
+	if err := c.ShouldBindJSON(&author); err != nil {
+		h.Logger.Sugar().Error("Falied to bing JSON: ", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := db.DB.Create(&author).Error; err != nil {
+		h.Logger.Sugar().Error("Falied to create author: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	h.Logger.Sugar().Info("Successful created reader: ", author)
+	c.JSON(http.StatusCreated, author)
+}
+
+func (h *Handler) CreateLoanHandler(c *gin.Context) {
+	var loan *models.Loan
+	if err := c.ShouldBindJSON(&loan); err != nil {
+		h.Logger.Sugar().Error("Falied to bing JSON: ", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err := db.DB.Create(&loan).Error; err != nil {
+		h.Logger.Sugar().Error("Falied to create loan: ", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	h.Logger.Sugar().Info("Successful created reader: ", loan)
+	c.JSON(http.StatusCreated, loan)
+
+}
--- internal/models/loan.go
+++ internal/models/loan.go
@@ -5,8 +5,9 @@ import "gorm.io/gorm"
 type Loan struct {
 	gorm.Model
 	BookID     uint
+	Book       Book `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	ReaderID   uint
+	Reader     Reader `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	LoanDate   string
 	ReturnDate string
 }
-
